cc4f2f766f7070991b748f2779688374864d87c4
pakitowka <whitesaint12@gmail.com>
2023-04-30 14:33:18 +0200

    language choose

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import requests
 from modules.acc_info_giver import get_url
 from modules.mail_sender import send_mail
 
+
 #make request
-request = requests.get(get_url())
+def news_language(command="&language=", language="en"):
+    request = requests.get(get_url()+command+language)
+    return request
 
 #get dict with data
-content = request.json()
+content = news_language().json()
 
 #get articles title and description and sent emails
 message = ""
 for article in content["articles"]:
     if article["title"] and article["description"] is not None:
-        message = message + article["title"] + "\n" + article["description"] + "\n"*3
+        message = message + article["title"] + "\n" + \
+                  article["description"] + "\n" +\
+                  article["url"] + 3 * "\n"
 
 send_mail(message.encode('utf-8'))
