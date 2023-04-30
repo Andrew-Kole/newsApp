6059bdcae864e4bf0c85800b2f9881323cad8729
pakitowka <whitesaint12@gmail.com>
2023-04-30 14:46:33 +0200

    adding subject to mail and sending just 20 news

--- main.py
+++ main.py
@@ -4,16 +4,32 @@ from modules.mail_sender import send_mail
 
 
 #make request
-def news_language(command="&language=", language="en"):
-    request = requests.get(get_url()+command+language)
+def news_language(language="en"):
+    """language is English by default, possible languages are
+    ar - arabic
+    de - german
+    en - english
+    es - spanish
+    fr - french
+    he - hebrew
+    it - italian
+    nl - niderlandian
+    no - norwegian
+    pt - portuguese
+    ru - russian
+    sv - swedish
+    ud - danish
+    zh - chinese
+    """
+    request = requests.get(get_url()+"&language="+language)
     return request
 
 #get dict with data
 content = news_language().json()
 
 #get articles title and description and sent emails
-message = ""
-for article in content["articles"]:
+message = "Subject: Today's news" + "\n"
+for article in content["articles"][:20]:
     if article["title"] and article["description"] is not None:
         message = message + article["title"] + "\n" + \
                   article["description"] + "\n" +\
